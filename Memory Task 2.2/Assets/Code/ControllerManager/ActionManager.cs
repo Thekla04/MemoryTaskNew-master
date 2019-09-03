d13e3a42de44d84890be69338d9b7e3775eaaf5a
Thekla04 <53309431+Thekla04@users.noreply.github.com>
2019-09-03 09:23:44 +0200

    VR

---
+++ Memory Task 2.2/Assets/Code/ControllerManager/ActionManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Valve.VR;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean myAction;
+    //public SteamVR_Action_Boolean grabAction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetActivationAction())
+        {
+            print("Activated " + handType);
+        }
+    }
+
+    public bool GetActivationAction()
+    {
+        return myAction.GetStateDown(handType);
+    }
+}
---
+++ Memory Task 2.2/Assets/Code/ControllerManager/ControllerClickObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerClickObject : MonoBehaviour
+{
+    public SteamVR_Input_Sources handyType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean clickAction;
+
+    private GameObject _collidingObject;
+    private GameObject _clickedObject;
+
+    private void SetCollidingObject(Collider col)
+    {
+        // cases in which it is not useful / possible to set the colliding object
+        // 1. You can not click something if you are already "holding" something
+        // 2. Or if the Object you wanna click has no rigidbody
+        // both don't seem relevant to us
+        if (_collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+
+        // set the colliding Object
+        _collidingObject = col.gameObject;
+    }
+    
+    // HIER SIND WIR -> ADD TRIGGER METHODS TODO
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
